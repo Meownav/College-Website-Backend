8ed4ebbab033ff01ea02ab41fcf3f656aae2d779
Meownav <manavchauhan124@gmail.com>
2023-03-12 15:35:07 +0530

    Routes for almni, atndnc.(empty), fclty & stud.

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
+const FacultyDB = require("./models/Faculty.jsx");
+const StudentDB = require("./models/Student.jsx");
+const AlumniDB = require("./models/Alumni.jsx");
+
 const server = express();
 server.use(cors());
 
@@ -10,42 +14,74 @@ server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
 
 mongoose
-  .connect(process.env.MONGO_URL)
+  .connect(process.env.MONGO_URL) // set MONGO_URL IN .env file.
   .then(console.log("Database connected."))
   .catch((err) => console.log(err));
 
-const professorSchema = new mongoose.Schema({
-  name: String,
-  designation: String,
-  subject: String,
-  email: String,
-  phone: Number,
-  join_date: Date,
-});
+// a page to add faculty will send the request here.
+server.post("/add-faculty", (req, res) => {
+  let faculty = new FacultyDB();
+  faculty.name = req.body.facultyName;
+  faculty.subject = req.body.facultySubject;
+  faculty.designation = req.body.facultyDesignation;
+  faculty.email = req.body.facultyEmail;
+  faculty.phone = req.body.facultyPhone;
+  faculty.join_date = req.body.facultyJoinDate;
 
-const Prof = mongoose.model("Professor", professorSchema);
-
-// a page to add professor will send the request here.
-server.post("/test1", (req, res) => {
-  let prof = new Prof();
-  prof.name = req.body.facultyName;
-  prof.subject = req.body.facultySubject;
-  prof.designation = req.body.facultyDesignation;
-  prof.email = req.body.facultyEmail;
-  prof.phone = req.body.facultyPhone;
-  prof.join_date = req.body.facultyJoinDate;
-  prof
+  faculty
     .save()
     .then((doc) => console.log(doc))
     .then(() => {
-      res.json({ message: "Successfully inserted to DB." });
+      res.json({ message: "Success" });
+    })
+    .catch((err) => {
+      res.json({ message: "Failed", reason: "" + err });
     });
 });
 
-server.get("/test1", (req, res) => {
-  Prof.find({}).then((docs) => res.json(docs));
+server.get("/get-all-faculty", (req, res) => {
+  FacultyDB.find({}).then((docs) => res.json(docs));
 });
 
-server.listen(5000, () => {
+server.post("/add-student", (req, res) => {
+  let student = new StudentDB();
+  student.name = req.body.studentName;
+  student.rollNumber = req.body.studentRollNum;
+  student.course = req.body.studentCourse;
+  student.email = req.body.studentEmail;
+  student.phone = req.body.studentPhone;
+  student.batchStart = req.body.batchStartYear;
+  student.batchEnd = req.body.batchEndYear;
+
+  student
+    .save()
+    .then((doc) => console.log(doc))
+    .then(() => res.json({ message: "Success" }))
+    .catch((err) => res.json({ message: "Failed", reason: "" + err }));
+});
+
+server.get("/get-all-students", (req, res) => {
+  StudentDB.find({}).then((docs) => res.json(docs));
+});
+
+server.post("/add-alumni", (req, res) => {
+  let alumni = new AlumniDB();
+  alumni.name = req.body.alumniName;
+  alumni.batch = req.body.alumniBatch;
+  alumni.qualifications = req.body.alumniCurrentWork;
+  alumni.currentWork = req.body.alumniCurrentWork;
+
+  alumni
+    .save()
+    .then((doc) => console.log(doc))
+    .then(() => res.json({ message: "Success" }))
+    .catch((err) => res.json({ message: "Failed", reason: "" + err }));
+});
+
+server.get("/get-all-alumni", (req, res) => {
+  AlumniDB.find({}).then((docs) => res.json(docs));
+});
+
+server.listen(process.env.PORT, () => {
   console.log("Server Started.");
 });
---
+++ models/Faculty.jsx
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const facultySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  designation: {
+    type: String,
+    required: true,
+  },
+  subject: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: Number,
+    required: true,
+  },
+  join_date: {
+    type: Date,
+    required: true,
+  },
+});
+
+const FacultyDB = mongoose.model("Faculty", facultySchema);
+
+module.exports = FacultyDB;
